fdd961ad6ae1462525b1d3ed79d5a18c9eed4c6a
Drago100cm <20230197@utsh.edu.mx>
2026-03-16 20:43:18 -0600

    fi

--- src/modules/app/components/Header.jsx
+++ src/modules/app/components/Header.jsx
@@ -37,9 +37,7 @@ const Header = ({ toggleSidebar, sidebarOpen }) => {
 
   return (
     <header className="header">
-<button onClick={toggleTheme}>
-  🌙
-</button>
+
       {/* LEFT */}
       <div className="header-left">
 
@@ -47,7 +45,7 @@ const Header = ({ toggleSidebar, sidebarOpen }) => {
 
         <div className="header-logo">
           <span className="logo-text">
-              <img id="logo" src="/logo_Dark.png" alt="Logo" />
+              <img id="logo" src="/logo.png" alt="Logo" />
           </span>
         </div>
 
@@ -136,7 +134,9 @@ const Header = ({ toggleSidebar, sidebarOpen }) => {
 
       {/* USER */}
       <div className="header-right">
-
+<button onClick={toggleTheme}>
+  🌙
+</button>
         <div
           className="header-profile"
           onClick={() => setShowUserMenu(!showUserMenu)}
